50f44e88c1e3da9ad602b92478ccacb33e7a006e
Niko <Niko@10.87.215.233>
2017-04-12 10:10:14 +0200

    annuncioCasa aggiornato

--- src/Casa/AnnuncioCasa.java
+++ src/Casa/AnnuncioCasa.java
@@ -5,6 +5,8 @@
  */
 package Casa;
 
+import Exceptions.CameraNonInseritaException;
+import Exceptions.CameraNonTrovataException;
 import java.util.ArrayList;
 
 /**
@@ -12,5 +14,40 @@ import java.util.ArrayList;
  * @author alberto
  */
 public class AnnuncioCasa {
+    private InfoCasa casa;
+    private String descrizioneAggiuntiva;
+    private int idAnnuncio;
+
+    public AnnuncioCasa(String descrizioneAggiuntiva, int idAnnuncio) {
+        this.descrizioneAggiuntiva = descrizioneAggiuntiva;
+        this.idAnnuncio = idAnnuncio;
+    }
     
+    public void creaCamera(int idCamera, int postiLetto, int postiLettoDisponibili) throws CameraNonInseritaException{
+        casa.creaCamera(idCamera, postiLetto, postiLettoDisponibili);
+    }
+    
+    public void rimuoviCamera(int idCamera) throws CameraNonTrovataException{
+        casa.rimuoviCamera(idAnnuncio);
+    }
+    
+    public void creaInfo(int m2, int nLocali, int nBagni, boolean cucinaSeparata){
+        casa = new InfoCasa(m2, nLocali, nBagni, cucinaSeparata);
+    }
+
+    public InfoCasa getCasa() {
+        return casa;
+    }
+
+    public String getDescrizioneAggiuntiva() {
+        return descrizioneAggiuntiva;
+    }
+
+    public int getIdAnnuncio() {
+        return idAnnuncio;
+    }
+
+    public void setDescrizioneAggiuntiva(String descrizioneAggiuntiva) {
+        this.descrizioneAggiuntiva = descrizioneAggiuntiva;
+    }
 }
