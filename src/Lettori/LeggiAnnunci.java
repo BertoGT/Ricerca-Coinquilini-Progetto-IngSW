8dd8331dafaae83749a17a22426c969a249f14b1
Niko <Niko@10.87.215.233>
2017-04-12 15:46:46 +0200

    leggiAnnunci scritto insieme al txt

--- src/Lettori/LeggiAnnunci.java
+++ src/Lettori/LeggiAnnunci.java
@@ -5,10 +5,44 @@
  */
 package Lettori;
 
+import Casa.AnnuncioCasa;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author fari
  */
 public class LeggiAnnunci {
+    private BufferedReader br;
+    private ArrayList<AnnuncioCasa> annunci;
     
+    public void leggiCase(){
+        annunci = new ArrayList<AnnuncioCasa>();
+        try {
+            br = new BufferedReader(new FileReader("file/annunci.txt"));
+            while(br.ready()){
+               String[] str = br.readLine().split("\t");
+               annunci.add(new AnnuncioCasa(str[0], Integer.parseInt(str[1]), Integer.parseInt(str[2])));
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(LeggiAnnunci.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(LeggiAnnunci.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public String stampaAnnunci(){
+        StringBuilder sb = new StringBuilder();
+        for (AnnuncioCasa a : annunci) {
+            sb.append(a);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 }
