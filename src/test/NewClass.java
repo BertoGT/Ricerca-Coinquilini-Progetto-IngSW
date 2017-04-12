48f281dff89392e0216e4fa42e80c0f7a32ed161
Niko <Niko@10.87.215.233>
2017-04-12 15:48:04 +0200

    test leggiannunci

---
+++ src/test/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import Lettori.LeggiAnnunci;
+
+/**
+ *
+ * @author Niko
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        LeggiAnnunci l = new LeggiAnnunci();
+        l.leggiCase();
+        l.stampaAnnunci();
+    }
+}
