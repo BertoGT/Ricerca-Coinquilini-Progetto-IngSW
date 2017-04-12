5067e7a860164cbf7e156d8eb18b71f5d535cf22
Alberto <alberto.groppi2@gmail.com>
2017-04-12 15:41:17 +0200

    test modifica algoritmo ricerca

--- src/RicercaAnnuncio/RicercaAnnuncio.java
+++ src/RicercaAnnuncio/RicercaAnnuncio.java
@@ -13,57 +13,72 @@ import java.util.ArrayList;
  * @author alberto
  */
 public class RicercaAnnuncio {
+
     private ArrayList<AnnuncioCasa> annunciTotali;
     private ParametriRicercaAnnuncio par;
 
-    public RicercaAnnuncio(ArrayList<AnnuncioCasa> annunciTotali, String citta, 
-            int metriQuadriMin, int metriQuadriMax, int numeroLocaliMin, 
+    public RicercaAnnuncio(ArrayList<AnnuncioCasa> annunciTotali, String citta,
+            int metriQuadriMin, int metriQuadriMax, int numeroLocaliMin,
             int numeroLocaliMax, int numeroBagni, int postiLettoMax, int costoMax) {
-        
+
         this.annunciTotali = annunciTotali;
-        par = new ParametriRicercaAnnuncio(citta, metriQuadriMin, metriQuadriMax, 
+        par = new ParametriRicercaAnnuncio(citta, metriQuadriMin, metriQuadriMax,
                 numeroLocaliMin, numeroLocaliMax, numeroBagni, postiLettoMax, costoMax);
     }
-    
+
     public ArrayList<AnnuncioCasa> eseguiRicerca() {
         ArrayList<AnnuncioCasa> result = new ArrayList<>();
         for (AnnuncioCasa a : annunciTotali) {
-            
-            if(!par.getCitta().equals(a.getCitta()))
+
+            if (!par.getCitta().equals(a.getCitta())) {
                 break;
+            }
             
-            if(a.getMetriQuadri() <= par.getMetriQuadriMin() && 
-                    a.getMetriQuadri() >= par.getMetriQuadriMax())
-                break;
-            
-            if(a.getnLocali() <= par.getNumeroLocaliMin() && 
-                    a.getnLocali() >= par.getNumeroLocaliMax())
-                break;
-            
-            if(a.getNumeroBagni() != par.getNumeroBagni())
-                break;
-            
-            if(a.getCosto() >= par.getCostoMax())
-                break;
-            
-            // Controllo che ci sia almeno una camera che soddisfa le richieste 
-            ArrayList<CameraDisponibile> camera = a.getCamere();
-            boolean flag = false;
-            for (CameraDisponibile cameraDisponibile : camera) {
-                if(cameraDisponibile.getPostiLettoDisponibili() > 0 &&
-                        cameraDisponibile.getPostiLetto() <= par.getPostiLettoMax()) {
-                    flag = true; // ho trovato almeno una camera che va bene
+            // se i m2 max sono 0, non inserisco il parametro, quindi salta il filtro
+            if (par.getMetriQuadriMax() != 0 ) {
+                if (a.getMetriQuadri() <= par.getMetriQuadriMin()
+                        && a.getMetriQuadri() >= par.getMetriQuadriMax()) {
                     break;
                 }
             }
-            
-            if(flag)
+            if(par.getNumeroLocaliMax() != 0) {
+                if (a.getnLocali() <= par.getNumeroLocaliMin()
+                        && a.getnLocali() >= par.getNumeroLocaliMax()) {
+                    break;
+                }
+            }
+
+            if(par.getNumeroBagni() != 0) {
+                if (a.getNumeroBagni() != par.getNumeroBagni()) {
+                    break;
+                }
+            }
+
+            if(par.getCostoMax() != 0) { 
+                if (a.getCosto() >= par.getCostoMax()) {
+                    break;
+                }
+            }
+
+            if(par.getPostiLettoMax() != 0) {
+                // Controllo che ci sia almeno una camera che soddisfa le richieste 
+                ArrayList<CameraDisponibile> camera = a.getCamere();
+                boolean flag = false;
+                for (CameraDisponibile cameraDisponibile : camera) {
+                    if (cameraDisponibile.getPostiLettoDisponibili() > 0
+                           && cameraDisponibile.getPostiLetto() <= par.getPostiLettoMax()) {
+                        flag = true; // ho trovato almeno una camera che va bene
+                        break;
+                    }
+                }
+
+                if (flag) {
+                    result.add(a);
+                }
+            } else 
                 result.add(a);
         }
         return result;
     }
-    
-    
-    
-    
+
 }
---
+++ src/test/TestRicercaSenzaLetturaFile.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import Casa.AnnuncioCasa;
+import Exceptions.CameraNonInseritaException;
+import RicercaAnnuncio.ParametriRicercaAnnuncio;
+import RicercaAnnuncio.RicercaAnnuncio;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author nicholas
+ */
+public class TestRicercaSenzaLetturaFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<AnnuncioCasa> annunci = new ArrayList<>();
+        AnnuncioCasa a1 = new AnnuncioCasa("prova", 1, 250);
+        
+        a1.creaInfo(100, 2, 1, true, "Pavia", "Via Ferrini, 77");
+        try {
+            a1.creaCamera(1, 2, 1);
+        } catch (CameraNonInseritaException ex) {
+            Logger.getLogger(TestRicercaSenzaLetturaFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        annunci.add(a1);
+        
+        AnnuncioCasa a2 = new AnnuncioCasa("prova", 1, 350);
+        a2.creaInfo(100, 2, 1, true, "Pavia", "Via Ferrini, 77");
+        try {
+            a2.creaCamera(1, 2, 1);
+        } catch (CameraNonInseritaException ex) {
+            Logger.getLogger(TestRicercaSenzaLetturaFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        annunci.add(a2);
+        
+        ArrayList<AnnuncioCasa> annunciRisultati;
+        annunciRisultati = new RicercaAnnuncio(annunci, "Pavia", 0, 0, 0, 0, 0, 0, 300).eseguiRicerca();
+        System.out.println("prova");
+    }
+    
+}
