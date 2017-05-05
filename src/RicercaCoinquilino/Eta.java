c06301cee7a904e34a049ff0c398a0ddc59341c3
Alberto <alberto.groppi2@gmail.com>
2017-05-05 12:30:50 +0200

    merge

--- src/RicercaCoinquilino/Eta.java
+++ src/RicercaCoinquilino/Eta.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -31,37 +31,3 @@ public class Eta extends ParametroRicercaCoinquilino{
     }
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package RicercaCoinquilino;
-
-import ProfiloUtente.DatiUtente;
-
-/**
- *
- * @author Niko
- */
-public class Eta extends ParametroRicercaCoinquilino{
-    
-    private int etaMin, etaMax;
-
-    public Eta(int stelle, int etaMin, int etaMax) {
-        super(stelle);
-        this.etaMin = etaMin;
-        this.etaMax = etaMax;
-    }
-
-    @Override
-    public float calcolaAffinità(DatiUtente utente) {
-        if(utente.getEta() <= etaMax && utente.getEta() >= etaMin)
-            return super.getStelle();
-        else 
-            return 0;
-    }
-    
-}
->>>>>>> origin/master
