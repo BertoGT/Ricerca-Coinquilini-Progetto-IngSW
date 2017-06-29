bcd57dd1f5343cdb180fd57921e305663a6b084f
Margherita <Margherita@10.65.32.122>
2017-06-29 16:15:40 +0200

    Modifiche servlet Registrazione

--- src/Servlet/RegistrationServlet.java
+++ src/Servlet/RegistrationServlet.java
@@ -5,11 +5,22 @@
  */
 package Servlet;
 
+
+import Casa.Citta;
+import ProfiloUtente.Facolta;
+import ProfiloUtente.Nazione;
+import ProfiloUtente.Occupazione;
+import ProfiloUtente.Sesso;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import sistema.Sistema;
 
 
 /**
@@ -27,8 +38,53 @@ public class RegistrationServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp); //To change body of generated methods, choose Tools | Templates.
+        
+        String nome = req.getParameter("nome");
+        String cognome = req.getParameter("cognome");
+        String email = req.getParameter("email");
+        String sesso = req.getParameter("sesso");
+        String dataDiNascita = req.getParameter("datadinascita");
+        String nazionalita = req.getParameter("nazionalita");
+        String facolta= req.getParameter("facolta");
+        String telefono = req.getParameter("cellulare");
+        String occupazione = req.getParameter("occupazione");
+        String fumatore = req.getParameter("fumatore");
+        String cuoco = req.getParameter("cuoco");
+        String sportivo = req.getParameter("sportivo");
+        String password = req.getParameter("password");
+        
+        String[] giornoMeseAnno = dataDiNascita.split("-");
+        int giorno = Integer.parseInt(giornoMeseAnno[2]);
+        int mese = Integer.parseInt(giornoMeseAnno[1]);
+        int anno = Integer.parseInt(giornoMeseAnno[0]);
+        
+        boolean fumatoreBoolean = false;
+        if(fumatore.equals("F")) 
+            fumatoreBoolean = true;
+        
+        boolean cuocoBoolean = false;
+        if(cuoco.equals("C")) 
+            cuocoBoolean = true;
+        
+        boolean sportivoBoolean = false;
+        if(sportivo.equals("S")) 
+            sportivoBoolean = true;
+
+        
+        int a=0;
+   
+        try {
+            Sistema sys= new Sistema();
+            sys.registrazioneUtente(nome, cognome, Sesso.valueOf(sesso), email, password, giorno, mese, anno, telefono, nazionalita, occupazione, facolta, fumatoreBoolean, cuocoBoolean, sportivoBoolean, Citta.valueOf(citta), false);
+        } catch (SQLException ex) {
+            Logger.getLogger(RegistrationServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        
     }
+    
     
     
     
