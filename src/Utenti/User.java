853e21bf20b0ce79ed497335a60336716fa855d8
cl428034 <cl428034@localhost>
2017-06-30 11:32:01 +0200

    aggiunta Javadoc package utenti

--- src/Utenti/User.java
+++ src/Utenti/User.java
@@ -37,18 +37,34 @@ public class User implements Power{
         return "\nACCESSO: " +this.dataOraAccesso.toString() + "\nUSER \nPOWER: " + this.power;
     }
 
+    /**
+     * 
+     * @return RITORNA L'ORA DI ACCESSO DELL'USER 
+     */
     public Calendar getDataOraAccesso() {
         return dataOraAccesso;
     }
 
+    /**
+     * 
+     * @param dataOraAccesso SETTA L'ORA DI ACCESSO DELL'USER
+     */
     public void setDataOraAccesso(Calendar dataOraAccesso) {
         this.dataOraAccesso = dataOraAccesso;
     }
 
+    /**
+     * 
+     * @return RITORNA IL PROFILE MANAGER DELL'UTENTE
+     */
     public ProfileManager getProfileManager() {
         return profileManager;
     }  
 
+    /**
+     * 
+     * @param profileManager SETTA IL PROFILE MANAGER DELL'UTENTE 
+     */
     public void setProfileManager(ProfileManager profileManager) {
         this.profileManager = profileManager;
     }
