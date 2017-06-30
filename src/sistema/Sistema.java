73398dfc7e580c90bcc4619bada2021ad42bb118
cl428034 <cl428034@localhost>
2017-06-30 11:12:39 +0200

    fine javadoc sistema

--- src/sistema/Sistema.java
+++ src/sistema/Sistema.java
@@ -87,15 +87,15 @@ public class Sistema {
      * @param giorno GIORNO DI NASCITA DELL'UTENTE
      * @param mese MESE DI NASCITA DELL'UTENTE
      * @param anno ANNO DI NASCITA DELL'UTENTE
-     * @param cellulare
-     * @param nazionalita
-     * @param occupazione
-     * @param facolta
-     * @param fumatore
-     * @param cuoco
-     * @param sportivo
-     * @param cittaDiRicerca
-     * @param potenzialeCoinquilino
+     * @param cellulare CELLULARE DELL'UTENETE
+     * @param nazionalita NAZIONALITA DELL'UTENTE
+     * @param occupazione OCCUPAZIONE DELL'UTENTE
+     * @param facolta FACOLTA DELL'UTENTE
+     * @param fumatore BOOLEANO CHE INDICA SE L'UTENTE FUMA
+     * @param cuoco BOOLEANO CHE INDICA SE L'UTENTE SA CUCINARE
+     * @param sportivo BOOLEAN CHE INDICA SE L'UTENTE FA SPORT
+     * @param cittaDiRicerca CITTA IN CUI L'UTENTE CERCA CASA
+     * @param potenzialeCoinquilino BOOLEANO CHE INDICA SE L'UTENTE È CANDIDATO COME COINQUILINO
      * @throws RegistrazioneException
      * @throws SQLException
      * @throws ParseException 
@@ -110,6 +110,7 @@ public class Sistema {
         this.bmUtente.inserisciInfoUtente(idUtente, dati);
         
     }
+   
     private void switchToUser(){
         this.setNullAll();
         this.user = new User();
@@ -125,110 +126,244 @@ public class Sistema {
         this.guest.setDataOraAccesso(dataOraAccesso);
     }
     
+    /**
+     * 
+     * @param costo PREZZO MAX CHE L'UTENTE VUOLE SPENDERE PER LA CASA
+     */
     public void setCostoMax(int costo) {
         this.parametriAnnuncio.setCostoMax(costo);
     }
+    
+    /**
+     * 
+     * @param citta CITTA IN CUI L'UTENTE CERCA CASA
+     */
     public void iniziaRicercaAnnunci(Citta citta){
         this.parametriAnnuncio = new ContenitoreParametriAnnuncio(citta);
     }
+    
+    /**
+     * 
+     * @param citta CITTA IN CUI L'UTENTE CERCA COINQUILINO 
+     */
     public void iniziaRicercaCoinquilini(Citta citta){
         this.parametriCoinquilino = new ContenitoreParametriCoinquilino(citta);
     }
+    
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA CHE L'UTENTE DA ALLA PRESENZA O MENO DELLA CUCINA SEPARATA
+     * @param cucinaSeparata BOOLEANO CHE INDICA SE L'UTENTE CERCA UNA CASA CON CUCINA SEPARATA 
+     */
     public void setParametroCucina(int stelle, boolean cucinaSeparata) {
         this.parametriAnnuncio.setParametroCucina(stelle, cucinaSeparata);
     }
     
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA CHE L'UTENTE DA ALLA DISTANZA DELLA CASA DAL CENTRO
+     * @param distanzaMax DISTANZA MAX CHE L'UTENTE VUOLE DELLA CASA DAL CENTRO
+     */
     public void setParametroDistCentro(int stelle, int distanzaMax) {
         this.parametriAnnuncio.setParametroDistCentro(stelle, distanzaMax);
     }
     
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA CHE L'UTENTE DA AL NUMERO DEI LOCALI PRESENTI IN CASA
+     * @param nLocali NUMERO DEI LOCALI CHE L'UTENTE RICERCA IN UNA CASA  
+     */
     public void setParametroNLocali(int stelle, int nLocali) {
         this.parametriAnnuncio.setParametroNLocali(stelle, nLocali);
     }
     
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA CHE L'UTENTE DA AL NUMERO DEI BAGNI PRESENTI IN CASA
+     * @param nBagni NUMERO DEI BAGNI CHE L'UTENTE RICERCA IN UNA CASA
+     */
     public void setParametroNBagni(int stelle, int nBagni) {
         this.parametriAnnuncio.setParametroNBagni(stelle, nBagni);
     }
     
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA CHE L'UTENTE DA AL SESSO PRESENTE IN CASA
+     * @param sesso SESSO DEGLI INQUILINI CHE L'UTENTE RICERCA IN UNA CASA
+     */
     public void setParametroSessoCasa(int stelle, HouseGenerality sesso) {
         this.parametriAnnuncio.setParametroSessoCasa(stelle, sesso);
     }
     
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA CHE L'UTENTE DA AL TIPO DI CAMERA CHE RICERCA IN UNA CASA
+     * @param postiLetto NUMERO DEI POSTI LETTO CHE L'UTENTE RICERCA IN UNA CAMERA DI UNA CASA
+     */
     public void setParametroTipoCamera(int stelle, int postiLetto) {
         this.parametriAnnuncio.setParametroTipoCamera(stelle, postiLetto);
     }
     
+    /**
+     * 
+     * @return RITORNA UN GUEST 
+     */
     public Guest getGuest() {
         return guest;
     }
 
+    /**
+     * 
+     * @return RITORNA UN USER  
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * 
+     * @return RITORNA I PARAMETRI MESSI DALL'UTENTE PER LA RICERCA ANNUNCIO
+     */
     public ContenitoreParametriAnnuncio getParametriAnnuncio() {
         return parametriAnnuncio;
     }
 
+    /**
+     * 
+     * @return RITORNA LA RICERCA ANNUNCIO 
+     */
     public RicercaAnnuncio getRicercaAnnuncio() {
         return ricercaAnnuncio;
     }
 
+    /**
+     * 
+     * @return RITORNA I PARAMETRI MESSI DALL'UTENTE PER LA RICERCA COINQUILINO 
+     */
     public ContenitoreParametriCoinquilino getParametriCoinquilino() {
         return parametriCoinquilino;
     }
-
+    
+    /**
+     * 
+     * @return RITORNA LA RICERCA COINQUILINO
+     */
     public RicercaCoinquilino getRicercaCoinquilino() {
         return ricercaCoinquilino;
     }
 
+    /**
+     * 
+     * @return RITORNA L'ORA DI ACCESSO DI UN UTENTE 
+     */
     public Calendar getDataOraAccesso() {
         return dataOraAccesso;
     }
     
+    /**
+     * 
+     * @throws SQLException
+     * @throws NessunAnnuncioException 
+     */
     public void ricercaAnnuncio() throws SQLException, NessunAnnuncioException{
         this.ricercaAnnuncio = new RicercaAnnuncio(this.parametriAnnuncio);
         this.annunciRisultanti = this.ricercaAnnuncio.eseguiRicerca();
     }
+    
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA CHE UN UTENTE DA ALLA CAPACITA O MENO DI UN COINQUILINO DI CUCINARE
+     * @param isCuoco BOOLEANO CHE INDICANO LA PREFERENZA SULLA CAPACITA O MENO DI UN COINQUILINO DI CUCINARE
+     */
     public void setParametroCuoco(int stelle, Boolean isCuoco){
         this.parametriCoinquilino.setParametroCuoco(stelle, isCuoco);
     }
     
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA CHE UN UTENTE DA ALLA FASCIA DI ETA DEL COINQUILINO RICERCATO
+     * @param etaMin ETA MINIMA DEL COINQUILINO RICERCATO
+     * @param etaMax ETA MAX DEL COINQUILINO RICERCATO
+     */
     public void setParametroEta(int stelle,int etaMin,int etaMax){
         this.parametriCoinquilino.setParametroEta(stelle, etaMin, etaMax);
     }
     
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA CHE L'UTENTE DA ALLA FACOLTA FREQUENTATA DAL COINQUILINO RICERCATO
+     * @param facolta FACOLTA DEL COINQUILINO RICERCATO 
+     */
     public void setParametroFacolta(int stelle,ProfiloUtente.Facolta facolta){
         this.parametriCoinquilino.setParametroFacolta(stelle, facolta);
     }
     
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA CHE UN UTENTE DA AL FATTO CHE IL COINQUILINO RICERCATO FUMI O MENO
+     * @param isFumatore BOOLEANO CHE INDICA SE IL COINQUILINO RICERCATO FUMA O NO 
+     */
     public void setParametroFumatore(int stelle,boolean isFumatore){
         this.parametriCoinquilino.setParametroFumatore(stelle, isFumatore);
     }
     
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA  CHE UN UTENTE DA ALLA NAZIONALITA DEL COINQUILINO
+     * @param nazionalita NAZIONALITA DEL COINQUILINO RICERCATO
+     */
     public void setParametroNazionalita(int stelle,ProfiloUtente.Nazione nazionalita){
         this.parametriCoinquilino.setParametroNazionalita(stelle, nazionalita);
     }
     
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA CHE UN UTENTE DA ALL'OCCUPAZIONE DEL COINQUILINO RICERCATO
+     * @param occupazione OCCUPAZIONE DEL COINQUILINO RICERCATO
+     */
     public void setParametroOccupazione(int stelle,ProfiloUtente.Occupazione occupazione){
         this.parametriCoinquilino.setParametroOccupazione(stelle, occupazione);
     } 
     
+    /**
+     * 
+     * @param stelle STELLE CHE INDICANO L'IMPORTANZA CHE L'UTENTE DA AL FATTO CHE IL COINQUILINO RICERCATO SIA SPORTIVO
+     * @param sportivo BOOLEANO CHE INDICA SE IL COINQUILINO RICERCATO DEVE ESSERE SPORTIVO O MENO
+     */
     public void setParametroSportivo(int stelle,boolean sportivo){
         this.parametriCoinquilino.setParametroSportivo(stelle, sportivo);
     }
+    
+    /**
+     * 
+     * @param sesso SESSO DEL COINQUILINO RICERCATO 
+     */
     public void setSesso(Sesso sesso) {
         this.parametriCoinquilino.setSesso(sesso);
     }
+    
+    /**
+     * 
+     * @throws SQLException
+     * @throws NessunAnnuncioException 
+     */
     public void ricercaCoinquilino() throws SQLException, NessunAnnuncioException {
         this.ricercaCoinquilino = new RicercaCoinquilino(this.parametriCoinquilino);
         this.coinquiliniRisultanti = this.ricercaCoinquilino.eseguiRicerca();
     }
 
+    /**
+     * 
+     * @return RITORNA GLI ANNUNCI RISULTANTI DALLA RICERCA 
+     */
     public ArrayList<AnnuncioRisultante> getAnnunciRisultanti() {
         return annunciRisultanti;
     }
-
+    
+    /**
+     * 
+     * @return RITORNA I COINQUILINI RISULTANTI DALLA RICERCA
+     */
     public ArrayList<CoinquilinoRisultante> getCoinquiliniRisultanti() {
         return coinquiliniRisultanti;
     }
