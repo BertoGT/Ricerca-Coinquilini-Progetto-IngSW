c2837077392bd72c369995cc103f692caff3fb73
BertoGT <alberto.groppi2@gmail.com>
2017-07-03 18:39:04 +0200

    Update HomePageServlet.java

--- src/Servlet/HomePageServlet.java
+++ src/Servlet/HomePageServlet.java
@@ -27,17 +27,23 @@ public class HomePageServlet extends HttpServlet{
         try {
             Sistema s = new Sistema();
             Cookie cookie = req.getCookies()[0]; 
-            int idUtente = Integer.parseInt(cookie.getName());
-            
+            int idUtente = Integer.parseInt(cookie.getName());        
             if(CookieStorage.getInstance().controllaPresenzaCookie(cookie)){
                 s.settaLoggato(idUtente);
+            } else {
+                cookie.setMaxAge(0); // il cookie non è più valido, dunque lo elimino
+                resp.addCookie(cookie);
+                settaNonLoggato(resp);
             }
         } catch (SQLException ex) {
             Logger.getLogger(HomePageServlet.class.getName()).log(Level.SEVERE, null, ex);
         } catch (NullPointerException ex) {
-            // utente non loggato
-        }
-        
+            settaNonLoggato(resp);
+        } 
+    }
+    
+    private void settaNonLoggato(HttpServletResponse response) {
+        // risposta da utente non loggato
     }
     
 }
