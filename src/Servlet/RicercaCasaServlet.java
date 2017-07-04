0768d8936965a516711d2715aa59421deb47731e
Margherita <Margherita@172.20.10.4>
2017-07-04 17:26:20 +0200

    Ultime modifiche alla servlet per la Ricerca Casa e aggiunta dinamica della navbar per utente loggato e non

--- src/Servlet/RicercaCasaServlet.java
+++ src/Servlet/RicercaCasaServlet.java
@@ -32,21 +32,23 @@ import sistema.Sistema;
 public class RicercaCasaServlet extends HttpServlet {
     
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
         try {
-            Cookie cookie = request.getCookies()[0]; 
+            Cookie cookie = req.getCookies()[0]; 
             if(CookieStorage.getInstance().controllaPresenzaCookie(cookie)){
                 // utente gia loggato.
-                String ricercaHtml = HtmlReader.htmlReader("RicercaCasaLoggato.html");
-                response.setStatus(200);
-                response.getWriter().println(ricercaHtml);
+                String headerLoggato = HtmlReader.htmlReader("headerLoggato.html");
+                String ricercaHtml = HtmlReader.htmlReader("RicercaCasa.html");
+                resp.setStatus(200);
+                resp.getWriter().println(headerLoggato + ricercaHtml);
             } else {
                 cookie.setMaxAge(0); // il cookie non è più valido, dunque lo elimino
-                response.addCookie(cookie);
-                settaNonLoggato(response);
+                resp.addCookie(cookie);
+                settaNonLoggato(resp);
             }
         } catch (NullPointerException ex) {
-            settaNonLoggato(response);
+            settaNonLoggato(resp);
         }
     }
 
@@ -69,65 +71,147 @@ public class RicercaCasaServlet extends HttpServlet {
         String cittaDiRicerca = req.getParameter("cittadiricerca");
         String costoMax = req.getParameter("costoMax");
         String cucinaSeparata = req.getParameter("cucina");
-        String importCucinaSeparata = req.getParameter("importanzacucinaseparata");
+        String importCucinaSeparata = req.getParameter("rating");
         String distanzaCentro = req.getParameter("distanzacentro");
-        String importDistanzaCentro = req.getParameter("importanzadistanzacentro");
+        String importDistanzaCentro = req.getParameter("ratinga");
         String numeroLocali= req.getParameter("numerolocali");
-        String importNumeroLocali = req.getParameter("importanzanumerolocali");
+        String importNumeroLocali = req.getParameter("ratingb");
         String numeroBagni = req.getParameter("numerobagni");
-        String importNumeroBagni = req.getParameter("importanzanumerobagni");
+        String importNumeroBagni = req.getParameter("ratingc");
         String sessoCoinquilini = req.getParameter("sessocoinquilini");
-        String importSesso = req.getParameter("importanzasesso");
+        String importSesso = req.getParameter("ratingd");
         String tipoCamera = req.getParameter("tipocamera");
-        String importTipoCamera = req.getParameter("importanzatipocamera");
+        String importTipoCamera = req.getParameter("ratinge");
         String forno = req.getParameter("forno");
-        String importForno = req.getParameter("importanzaforno");
+        String importForno = req.getParameter("ratingf");
         String lavatrice = req.getParameter("lavatrice");
-        String importLavatrice = req.getParameter("importanzalavatrice");
+        String importLavatrice = req.getParameter("ratingg");
         String lavastoviglie = req.getParameter("lavastoviglie");
-        String importLavastoviglie = req.getParameter("importanzalavastoviglie");
+        String importLavastoviglie = req.getParameter("ratingn");
         String condizionatore = req.getParameter("condizionatore");
-        String importCondizionatore= req.getParameter("importanzacondizionatore");
+        String importCondizionatore= req.getParameter("ratingh");
         String aspirapolvere = req.getParameter("aspirapolvere");
-        String importAspirapolvere = req.getParameter("importanzaaspirapolvere");
+        String importAspirapolvere = req.getParameter("ratingi");
         String asciugatrice = req.getParameter("asciugatrice");
-        String importAsciugatrice = req.getParameter("importanzaasciugatrice");
+        String importAsciugatrice = req.getParameter("ratingl");
         String microonde = req.getParameter("microonde");
-        String importMicroonde = req.getParameter("importanzamicroonde");
+        String importMicroonde = req.getParameter("ratingm");
         
-         
-        boolean cucinaSeparataBoolean = true;
-        try{
-            if(cucinaSeparata.equals("null")) 
-                cucinaSeparataBoolean = false;
-        } catch (NullPointerException ex) {
-            cucinaSeparataBoolean = false;
-        }
+        
         
         Sistema sys= new Sistema();
         sys.iniziaRicercaAnnunci(Citta.valueOf(cittaDiRicerca));
-        sys.setCostoMax(Integer.parseInt(costoMax));
-        sys.setParametroCucina(Integer.parseInt(importCucinaSeparata), cucinaSeparataBoolean);
-        sys.setParametroDistCentro(Integer.parseInt(importDistanzaCentro), Integer.parseInt(distanzaCentro));
-        sys.setParametroNLocali(Integer.parseInt(importNumeroLocali), Integer.parseInt(numeroLocali));
-        sys.setParametroNBagni(Integer.parseInt(importNumeroBagni), Integer.parseInt(numeroBagni));
-        sys.setParametroSessoCasa(Integer.parseInt(importSesso), HouseGenerality.valueOf(sessoCoinquilini));
-        sys.setParametroTipoCamera(Integer.parseInt(importTipoCamera), Integer.parseInt(tipoCamera));
-        sys.setParametroElettrodomestico(Integer.parseInt(importForno), ElettroDomestico.valueOf(forno));
-        sys.setParametroElettrodomestico(Integer.parseInt(importLavatrice), ElettroDomestico.valueOf(lavatrice));
-        sys.setParametroElettrodomestico(Integer.parseInt(importLavastoviglie), ElettroDomestico.valueOf(lavastoviglie));
-        sys.setParametroElettrodomestico(Integer.parseInt(importCondizionatore), ElettroDomestico.valueOf(condizionatore));
-        sys.setParametroElettrodomestico(Integer.parseInt(importAsciugatrice), ElettroDomestico.valueOf(asciugatrice));
-        sys.setParametroElettrodomestico(Integer.parseInt(importAspirapolvere), ElettroDomestico.valueOf(aspirapolvere));
-        sys.setParametroElettrodomestico(Integer.parseInt(importMicroonde), ElettroDomestico.valueOf(microonde));
+        
+        
+        try {
+            if(!importCucinaSeparata.equals("null")) {
+                boolean cucinaSeparataBoolean = true;
+                try{
+                    if(cucinaSeparata.equals("null")) 
+                        cucinaSeparataBoolean = false;
+                } catch (NullPointerException ex) {
+                    cucinaSeparataBoolean = false;
+                } finally {
+                    sys.setParametroCucina(Integer.parseInt(importCucinaSeparata), cucinaSeparataBoolean);
+                }
+            }
+        } catch (NullPointerException ex) {}
+        
+        try {
+            if(!importDistanzaCentro.equals("null")) {
+                 sys.setParametroDistCentro(Integer.parseInt(importDistanzaCentro.trim()), Integer.parseInt(distanzaCentro.trim()));
+            }
+        } catch (NullPointerException | NumberFormatException ex) {}
+        
+        
+        try {
+            if(!importNumeroLocali.equals("null")) {
+                sys.setParametroNLocali(Integer.parseInt(importNumeroLocali), Integer.parseInt(numeroLocali));
+            }
+        } catch (NullPointerException ex) {}
+        
+        try {
+            if(!importNumeroBagni.equals("null")) {
+                sys.setParametroNBagni(Integer.parseInt(importNumeroBagni), Integer.parseInt(numeroBagni));
+            }
+        } catch (NullPointerException ex) {}
+        
+        try {
+            if(!importSesso.equals("null")) {
+                sys.setParametroSessoCasa(Integer.parseInt(importSesso), HouseGenerality.valueOf(sessoCoinquilini));
+            }
+        } catch (NullPointerException ex) {}
+        
+        try {
+            if(!importTipoCamera.equals("null")) {
+                sys.setParametroTipoCamera(Integer.parseInt(importTipoCamera), Integer.parseInt(tipoCamera));
+            }
+        } catch (NullPointerException ex) {}
+        
+        try {
+            if(!importForno.equals("null")) {
+                if(!forno.equals("null"))  
+                    sys.setParametroElettrodomestico(Integer.parseInt(importForno), ElettroDomestico.FORNO);
+            }
+        } catch (NullPointerException ex) {}
+        
+        try {
+            if(!importLavatrice.equals("null")) {
+                if(!lavatrice.equals("null"))  
+                    sys.setParametroElettrodomestico(Integer.parseInt(importLavatrice), ElettroDomestico.LAVATRICE);
+            }
+        } catch (NullPointerException ex) {}
+        
+        try {
+            if(!importLavastoviglie.equals("null")) {
+                if(!lavastoviglie.equals("null"))  
+                    sys.setParametroElettrodomestico(Integer.parseInt(importLavastoviglie), ElettroDomestico.LAVASTOVIGLIE);
+            }
+        } catch (NullPointerException ex) {}
+        
+        try {
+            if(!importCondizionatore.equals("null")) {
+                if(!condizionatore.equals("null"))  
+                    sys.setParametroElettrodomestico(Integer.parseInt(importCondizionatore), ElettroDomestico.CONDIZIONATORE);
+            }
+        } catch(NullPointerException ex) {}
+        
+        try{
+            if(!importAspirapolvere.equals("null")) {
+                if(!aspirapolvere.equals("null"))  
+                    sys.setParametroElettrodomestico(Integer.parseInt(importAspirapolvere), ElettroDomestico.ASPIRAPOLVERE);
+            }
+        } catch (NullPointerException ex) {}
+        
+        try {
+            if(!importAsciugatrice.equals("null")) {
+                if(!asciugatrice.equals("null"))  
+                    sys.setParametroElettrodomestico(Integer.parseInt(importAsciugatrice), ElettroDomestico.ASCIUGATRICE);
+            }
+        } catch (NullPointerException ex) {}
+        
+        try{
+            if(!importMicroonde.equals("null")) {
+                if(!microonde.equals("null"))  
+                    sys.setParametroElettrodomestico(Integer.parseInt(importMicroonde), ElettroDomestico.MICROONDE);
+            }
+        } catch (NullPointerException ex) {}
+        
+        try {
+            sys.setCostoMax(Integer.parseInt(costoMax));
+        } catch (NumberFormatException ex) {}
+        
         sys.ricercaAnnuncio();
+        
     }
     
-    private void settaNonLoggato(HttpServletResponse response) throws IOException {
-        String ricercaHtml = HtmlReader.htmlReader("RicercaCasaNonLoggato.html");
-        response.setStatus(200);
-        response.getWriter().println(ricercaHtml);
+    private void settaNonLoggato(HttpServletResponse resp) throws FileNotFoundException, IOException {
+        String headerNonLoggato = HtmlReader.htmlReader("headerNonLoggato.html");
+        String ricercaHtml = HtmlReader.htmlReader("RicercaCasa.html");
+        resp.setStatus(200);
+        resp.getWriter().println(headerNonLoggato + ricercaHtml);
     }
+    
     
     
 }
