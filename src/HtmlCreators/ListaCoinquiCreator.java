30c5b13a3b334387656a17d7810cf0aaee90188c
Marco La Salvia <marco.lasalvia01@universitadipavia.it>
2017-07-17 09:32:22 +0200

    Revisione interfaccia web

--- src/HtmlCreators/ListaCoinquiCreator.java
+++ src/HtmlCreators/ListaCoinquiCreator.java
@@ -65,7 +65,7 @@ public class ListaCoinquiCreator {
             sb.append(co.getUtente().getIdUtente()).append("\">");
             sb.append("<div class=\"w3-main w3-white\" style=\"margin:2% 10%; padding: 1% 2% 2% 2%; width:auto; text-align:left; opacity:0.95;\">");
             sb.append("<div class=\"w3-light-grey\">\n" +
-                      "<div class=\"w3-container w3-blue w3-center\" style=\"width:"+co.getPunteggio()+"%\">Affinita' : "+String.format("%.1f",co.getPunteggio())+"%</div>\n" +
+                      "<div class=\"w3-container w3-blue w3-center\" style=\"width:100%\">Affinita' : "+String.format("%.1f",co.getPunteggio())+"%</div>\n" +
                       "</div>");
             //sb.append("<div style=\"text-align:center;float:right; font-weight:bold; font-size:20;\">Affinita'<br>");
             //sb.append(co.getPunteggio()).append("%</div>");
